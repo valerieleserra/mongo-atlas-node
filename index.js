eddb7747f89b854dbb2b0f63fd919c7495602268
valerie leserra <valeriemjade@valeries-MacBook-Pro.local>
2021-07-08 14:46:44 -0400

    connection to mongodb ok

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//
+const mongoose = require('mongoose')
+
+mongoose
+.connect('mongodb+srv://valml:scully12@cluster0.bplbj.mongodb.net/test' , {useNewUrlParser:true}) //connect takes 2 things ('connectionstring' , 
+.then(() => console.log('We are connected to Mongo'))
+.catch(err => console.log('We are not connected to Mongo', err))
+
+//run JS file node .
